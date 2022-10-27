2831981b5220505ddcf2ca8dddffdd55dfc74cf6
jhoxani <juancdiazg@gmail.com>
2022-10-26 21:49:26 -0300

    avance del ckedit

--- appMVT/models.py
+++ appMVT/models.py
@@ -2,25 +2,30 @@ from django.db import models
 from unittest.util import _MAX_LENGTH
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
+
 class Familiar(models.Model):
-    
+
     def __str__(self):
-      return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Edad: {self.edad} - Fecha de Nacimiento: {self.fechaNacimiento}"
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Edad: {self.edad} - Fecha de Nacimiento: {self.fechaNacimiento}"
 
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     edad = models.IntegerField()
     fechaNacimiento = models.DateField(null=True)
-    
-""" class BlogFamiliar(models.Model):
-    
+
+
+class BlogFamiliar(models.Model):
+
     def __str__(self):
-      return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Edad: {self.edad} - Fecha de Nacimiento: {self.fechaNacimiento}"
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Edad: {self.edad} - Fecha de Nacimiento: {self.fechaNacimiento}"
 
     titulo = models.CharField(max_length=30)
     sub_titulo = models.CharField(max_length=30)
-    contenido = models.IntegerField()
-    fechaNacimiento = models.DateField(null=True) """
+    descripcion = RichTextField()
+    autor = models.CharField(max_length=30)
+    fechaCreacion = models.DateField(null=True)
+    imagen = models.ImageField(upload_to='imagenes', null=True, blank=True)
